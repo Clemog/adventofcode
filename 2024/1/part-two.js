6966b41037f599c56648c548ff475b27fed6189f
Clément <clem.auger@hotmail.fr>
2024-12-08 18:07:46 +0100

    feat: day 1

---
+++ 2024/1/part-two.js
@@ -0,0 +1,22 @@
+import { input } from "./input.js";
+
+const A = [];
+const B = [];
+
+input.map((line) => {
+  const [a, b] = line.split("   ");
+  A.push(+a);
+  B.push(+b);
+});
+
+const total = A.reduce((acc, a, i) => {
+  let occ = 0;
+  for (const b of B) {
+    if (b === a) {
+      occ++;
+    }
+  }
+  return acc + a * occ;
+}, 0);
+
+console.log(total);
